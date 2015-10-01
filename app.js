68138c20bfec8dbefde44b19b23f2e03e5ff4359
Brandon Ashcraft <keyman1986@gmail.com>
2015-10-01 15:19:29 -0600

    phase 2 complete

--- app.js
+++ app.js
@@ -2,25 +2,60 @@ var app = angular.module('racingFrogs', []);
 app.controller('MainController', MainController);
 //No need to change anything above this line.
 
-function MainController() {
+function MainController($timeout) {
     var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
     
     vm.joe = new Guy("Joe",100);
     vm.bob = new Guy("Bob",150);
     vm.bank = 200;
+    var _racing = false;
+    
+       
     
     vm.frogs = [
-        {name: "Tom", posX: 33},
-        {name: "Dick", posX: 33},
-        {name: "Harry", posX: 33}
+        {name: "Tom", posX: 36},
+        {name: "Dick", posX: 36},
+        {name: "Harry", posX: 36}
         ];
     
-    vm.race = function(){
-        vm.frogs.forEach(function(){
-            vm.frogs.posX += 1;
-            console.log("Hi");
+    vm.start = function(){
+        _racing = true;
+        _raceManager();
+    }
+   
+    function _checkWinners(){
+        vm.frogs.forEach(function(frog){
+            if(frog.posX >= 69){
+                alert("Race complete");
+                _racing = false;
+            }
+        })
+    }
+    
+    
+    function _move(){
+        vm.frogs.forEach(function(frog){
+            var random = Math.floor(Math.random()*5);
+            frog.posX += random;
         });
     };
+    
+    
+    function _raceManager(){
+        _checkWinners();
+        if(!_racing){
+            return;
+        }
+        _move();
+        $timeout(_raceManager, 50);
+    }
+    
+    vm.reset = function(){
+        vm.frogs.forEach(function(frog){
+            frog.posX = 36;
+        })
+    };
+  
     
     function Guy(name, startingCash) {
         this.name = name;
